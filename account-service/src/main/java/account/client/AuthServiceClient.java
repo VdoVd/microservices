7c5ea3f6f5a01c8a6f51b6e82a25833204d9e5a1
hello-express <liuhaobin486@gmail.com>
2022-10-10 11:53:59 +0800

    init microservices

---
+++ account-service/src/main/java/account/client/AuthServiceClient.java
@@ -0,0 +1,16 @@
+package account.client;
+
+import account.domain.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.print.attribute.standard.Media;
+import java.awt.*;
+
+@FeignClient(name = "auth-service")
+public interface AuthServiceClient {
+    @RequestMapping(method = RequestMethod.POST,value = "/uaa/users",consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    void createUser(User user);
+}
