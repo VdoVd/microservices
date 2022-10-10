a54e5aa929056c4fe45bcfd1a45405872838f88c
hello-express <liuhaobin486@gmail.com>
2022-10-10 18:16:28 +0800

    'config

---
+++ auth-service/src/main/java/config/OAuth2AuthorizationConfig.java
@@ -0,0 +1,62 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    private TokenStore tokenStore=new InMemoryTokenStore();
+    private final String NOOP_PASSWORD_ENCODE="{noop}";
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MongoUserDetailsService userDetailsService;
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)throws Exception{
+        clients.inMemory()
+                .withClient("browser")
+                .authorizedGrantTypes("refresh_token","password")
+                .scopes("ui")
+                .and()
+                .withClient("account-service")
+                .secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
+                .authorizedGrantTypes("client_credentials","refresh_token")
+                .scopes("server")
+                .and()
+                .withClient("statistics-service")
+                .secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
+                .authorizedGrantTypes("client_credentials","refresh_token")
+                .scopes("server")
+                .and()
+                .withClient("notification-service")
+                .secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
+                .authorizedGrantTypes("client_credentials","refresh_token")
+                .scopes("server");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpointsConfigurer)throws Exception{
+        endpointsConfigurer
+                .tokenStore(tokenStore)
+                .authenticationManager(authenticationManager)
+                .userDetailsService(use)
+    }
+}
